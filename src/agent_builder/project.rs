14ae73a02acbd8133514abd998e4ba8c2f4d29fc
analyzer1 <jpickard@analyzer1.com>
2025-08-30 20:45:54 -0400

    feat: push template files from mod agent_builder

--- src/agent_builder/project.rs
+++ src/agent_builder/project.rs
@@ -1,20 +1,45 @@
-use std::{fs, io, path::Path};
+use std::{fs, io::Error, path::PathBuf, str::FromStr};
 
-const AGENT_WORKSPACE_DIR: &str = ".cargo-ai/agents";
+use crate::templates::*;
 
-pub fn create_new_agent_project(agent_name: &str) -> Result<(), io::Error> {
+const AGENTS_WORKSPACE_DIRECTORY: &str = ".cargo-ai/agents";
 
-    create_agent_workspace()?;
+pub fn create_new_agent_project(agent_name: &str) -> Result<(), Error> {
+
+    create_agent_workspace(agent_name)?;
+    load_agent_workspace(agent_name)?;
 
     Ok(())
 }
 
-fn create_agent_workspace() -> Result<(), io::Error> {
+fn get_agent_workspace_director_path(agent_name: &str) -> PathBuf {
+    let agent_workspace_directory = format!("{AGENTS_WORKSPACE_DIRECTORY}/{agent_name}");
 
-    let agent_workspace_directory = Path::new(AGENT_WORKSPACE_DIR);
+    PathBuf::from_str(&agent_workspace_directory).expect("Unable to create agent path buffer.")
+}
+
+fn create_agent_workspace(agent_name: &str) -> Result<(), Error> {
+
+    let agent_workspace_directory = get_agent_workspace_director_path(agent_name);
+
     if !agent_workspace_directory.exists() {
         fs::create_dir_all(agent_workspace_directory)?;
     }
     
     Ok(()) 
+}
+
+fn load_agent_workspace(agent_name: &str) -> Result<(), Error> {
+
+   let agent_workspace_directory_path = get_agent_workspace_director_path(agent_name); 
+
+   let mut build_rs_path = agent_workspace_directory_path.clone();
+   build_rs_path.push(BUILD_RS_NAME);
+   std::fs::write(build_rs_path, BUILD_RS_TEMPLATE)?;
+
+   let mut agentcfg_path = agent_workspace_directory_path.clone();
+    agentcfg_path.push(AGENTCFG_NAME);
+    std::fs::write(agentcfg_path, AGENTCFG_TEMPLATE)?;
+
+    Ok(())
 }
---
+++ src/templates/mod.rs
@@ -0,0 +1,5 @@
+pub const BUILD_RS_NAME: &str = "build.rs";
+pub const BUILD_RS_TEMPLATE: &str = include_str!("build.rs");
+
+pub const AGENTCFG_NAME: &str = ".agentcfg";
+pub const AGENTCFG_TEMPLATE: &str = include_str!(".agentcfg");
